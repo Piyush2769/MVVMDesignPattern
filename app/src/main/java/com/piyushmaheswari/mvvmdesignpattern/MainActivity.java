1bd967560fc7ff51d8fe51ba73deca7153201f6d
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-10 12:46:23 +0530

    Login completed

--- app/src/main/java/com/piyushmaheswari/mvvmdesignpattern/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/mvvmdesignpattern/MainActivity.java
@@ -1,13 +1,37 @@
 package com.piyushmaheswari.mvvmdesignpattern;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.piyushmaheswari.mvvmdesignpattern.Interface.LoginResultCallback;
+import com.piyushmaheswari.mvvmdesignpattern.databinding.ActivityMainBinding;
+import com.piyushmaheswari.mvvmdesignpattern.viewmodel.LoginViewModel;
+import com.piyushmaheswari.mvvmdesignpattern.viewmodel.LoginViewModelFactory;
+
+import es.dmoral.toasty.Toasty;
+
+public class MainActivity extends AppCompatActivity implements LoginResultCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setViewModel(ViewModelProviders.of(this,new LoginViewModelFactory(this))
+        .get(LoginViewModel.class));
+
+
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        Toasty.success(this,"Login Success", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFailure(String message) {
+        Toasty.error(this,"Login Failure", Toast.LENGTH_SHORT).show();
     }
 }
